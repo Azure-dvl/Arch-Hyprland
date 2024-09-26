3102eebbf0b1e37c986267fca9f9a005c1ec4bcb
Azure-dvl <subarachisekai@gmail.com>
2024-09-26 01:24:55 -0400

    New Config

---
+++ .config/hypr/config/scripts/captureArea.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+grim -g "$(slurp)" /run/media/azure/ALMACEN/Pictures/Screenshots/$(date +'%s_grim.png')
---
+++ .config/hypr/config/scripts/captureScreen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grim -g "$(slurp -o)" /run/media/azure/ALMACEN/Pictures/Screenshots/$(date +'%s_grim.png')
